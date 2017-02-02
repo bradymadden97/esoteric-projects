bc87aa0afdbb9f790e829a5788bfa52c905df5de
Brady Madden <bradymadden97@gmail.com>
2017-02-02 17:11:13 -0500

    fixed minimizer and reformatted .bf

--- brainfuck-minimizer.py
+++ brainfuck-minimizer.py
@@ -1,7 +1,17 @@
 import re
-file = open('bf.txt', 'r')
-nf = open('bfnew.txt', 'a')
+filename = input("Enter brainfuck filename: ")
+file = open(filename, 'r+')
 f = file.readlines()
+file.seek(0)
+file.truncate()
+file.close()
+nf = open(filename, 'a')
+line = ""
+counter = 0
 for each in f:
-    line = re.sub('[^.,\[\]<>\-+]+', "", each).replace(" ", "").replace("\n", "")
-    nf.write(line)
+    for char in each:
+        line += re.sub('[^.,\[\]<>\-+]+', "", char).replace(" ", "").replace("\n", "")
+        counter += 1
+        if counter % 75 == 0:
+            line += '\n'
+nf.write(line)
